8551abd81cdef3939a59bdec64682f31dff35974
HoangDinh <144801116+changcodont@users.noreply.github.com>
2024-12-09 17:46:00 +0700

    a
    a

--- DUANSUA2/src/main/java/JAVA6/service/HistoryService.java
+++ DUANSUA2/src/main/java/JAVA6/service/HistoryService.java
@@ -3,6 +3,7 @@ package JAVA6.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import JAVA6.Model.OrderModel;
@@ -10,10 +11,20 @@ import JAVA6.repository.OrderRepository;
 
 @Service
 public class HistoryService {
-	 @Autowired
-	    private OrderRepository orderRepository;
+	@Autowired
+	private OrderRepository orderRepository;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
-	    public List<OrderModel> getbyIdOrders(int id) {
-	        return orderRepository.findbyId(id); // Phương thức findAll() sẽ trả về danh sách tất cả các đơn hàng từ cơ sở dữ liệu
-	    }
+	public List<OrderModel> getbyIdOrders(int id) {
+		return orderRepository.findbyId(id); // Phương thức findAll() sẽ trả về danh sách tất cả các đơn hàng từ cơ sở
+												// dữ liệu
+	}
+
+	public void cancelOrder(Long orderId, String cancelReason) {
+		String sql = "UPDATE Orders SET orderstatus_id = 8, cancel_reason = ? WHERE id = ?";
+
+		// Cập nhật trạng thái đơn hàng thành "Đã hủy" và lưu lý do hủy
+		jdbcTemplate.update(sql, cancelReason, orderId);
+	}
 }
